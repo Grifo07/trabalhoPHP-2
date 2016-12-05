77b6bfdd2ef9c83744f9ac9c6c0cf5e1bdf15c30
Grifo07 <egrifo@gmail.com>
2016-12-05 13:04:04 +0000

    nada

--- config/init.php
+++ config/init.php
@@ -10,20 +10,20 @@
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
-  
+  //sdjfhsdkf
   $smarty = new Smarty;
   $smarty->template_dir = $BASE_DIR . 'templates/';
   $smarty->compile_dir = $BASE_DIR . 'templates_c/';
   $smarty->assign('BASE_URL', $BASE_URL);
-  
-  $smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);  
+
+  $smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);
   $smarty->assign('FIELD_ERRORS', $_SESSION['field_errors']);
   $smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
   $smarty->assign('FORM_VALUES', $_SESSION['form_values']);
   $smarty->assign('USERNAME', $_SESSION['username']);
-  
+
   unset($_SESSION['success_messages']);
-  unset($_SESSION['error_messages']);  
+  unset($_SESSION['error_messages']);
   unset($_SESSION['field_errors']);
   unset($_SESSION['form_values']);
 ?>
