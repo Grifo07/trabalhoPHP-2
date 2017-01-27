c4a8219823f69863edb1de5caf364546ffad88c3
rquintao <rafaelerquintao@gmail.com>
2017-01-27 13:07:36 +0000

    asda

--- templates_c/a479f1619762d53568717af2d25edbb2d445e071.file.listSquare.tpl.php
+++ templates_c/a479f1619762d53568717af2d25edbb2d445e071.file.listSquare.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2017-01-18 14:40:40
+<?php /* Smarty version Smarty-3.1.15, created on 2017-01-27 12:05:36
          compiled from "/usr/users2/mieec2012/ee12113/public_html/trabalhosSiem/trabalhoPHP-2/templates/products/listSquare.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:329776875878d1886a9b68-13907037%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'a479f1619762d53568717af2d25edbb2d445e071' => 
     array (
       0 => '/usr/users2/mieec2012/ee12113/public_html/trabalhosSiem/trabalhoPHP-2/templates/products/listSquare.tpl',
-      1 => 1484711531,
+      1 => 1485476578,
       2 => 'file',
     ),
   ),
@@ -19,12 +19,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_5878d1888cffc8_21626968',
   'variables' => 
   array (
-    'products' => 0,
+    'destaques' => 0,
     'BASE_URL' => 0,
-    'product' => 0,
+    'destaque' => 0,
     'results' => 0,
     'cid' => 0,
     'con' => 0,
+    'novidades' => 0,
+    'novidade' => 0,
+    'promocoes' => 0,
+    'promocao' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -42,31 +46,38 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 
 
 
-		<center><div class="coluna" style="width: 70%;">
+		<center><div class="coluna" style="width: 100%;">
 				<h2 style="text-align: center;">DESTAQUE</h2>
 				<div class="linha">
 				
-				<?php  $_smarty_tpl->tpl_vars['product'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['product']->_loop = false;
- $_from = $_smarty_tpl->tpl_vars['products']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
-foreach ($_from as $_smarty_tpl->tpl_vars['product']->key => $_smarty_tpl->tpl_vars['product']->value) {
-$_smarty_tpl->tpl_vars['product']->_loop = true;
+				<?php  $_smarty_tpl->tpl_vars['destaque'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['destaque']->_loop = false;
+ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['destaques']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+ $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']=-1;
+foreach ($_from as $_smarty_tpl->tpl_vars['destaque']->key => $_smarty_tpl->tpl_vars['destaque']->value) {
+$_smarty_tpl->tpl_vars['destaque']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['destaque']->key;
+ $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']++;
 ?>
+				  <?php if ($_smarty_tpl->getVariable('smarty')->value['foreach']['foo']['index']==4) {?>
+				    <?php break 1?>
+				  <?php }?>	
 				
 						<div class="card">
 						  <a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
+pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['destaque']->value['id'];?>
 "><img src="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-<?php echo $_smarty_tpl->tpl_vars['product']->value['photo'];?>
+<?php echo $_smarty_tpl->tpl_vars['destaque']->value['photo'];?>
 " alt="Avatar" style="width:100%"></a>
 						  <div class="containercard">
-						    <h4><b><?php echo $_smarty_tpl->tpl_vars['product']->value['nome'];?>
+						    <h4><b><?php echo $_smarty_tpl->tpl_vars['destaque']->value['nome'];?>
 </b></h4> 
-						    <p><strong><?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						    <p><strong><?php echo $_smarty_tpl->tpl_vars['destaque']->value['preco'];?>
  €</strong></p> 
 						  </div>
 
-						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
-,<?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['destaque']->value['id'];?>
+,<?php echo $_smarty_tpl->tpl_vars['destaque']->value['preco'];?>
 );" class="botaocompra"><span><i class="fa fa-cart-plus fa-lg"></i></span></button>
 
 						</div>
@@ -93,22 +104,23 @@ if (!$_smarty_tpl->tpl_vars['con']->_loop) {
 				
 				</div>
 				<div class="linha" style="margin-right: 1%; margin-bottom: 1%">
-				<a href="#" style="float: right; color: black;">Mostrar todos >></a>
+				<a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+pages/products/listProducts.php?ex=1" style="float: right; color: black;">Mostrar todos >></a>
 				</div>
 
 
 		</div></center>
 
-		<center><div class="coluna" style="width: 70%;">
+		<center><div class="coluna" style="width: 100%;">
 				<h2 style="text-align: center;">NOVIDADES</h2>
 				<div class="linha">
-				<?php  $_smarty_tpl->tpl_vars['product'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['product']->_loop = false;
+				<?php  $_smarty_tpl->tpl_vars['novidade'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['novidade']->_loop = false;
  $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
- $_from = $_smarty_tpl->tpl_vars['products']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+ $_from = $_smarty_tpl->tpl_vars['novidades']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
  $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']=-1;
-foreach ($_from as $_smarty_tpl->tpl_vars['product']->key => $_smarty_tpl->tpl_vars['product']->value) {
-$_smarty_tpl->tpl_vars['product']->_loop = true;
- $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['product']->key;
+foreach ($_from as $_smarty_tpl->tpl_vars['novidade']->key => $_smarty_tpl->tpl_vars['novidade']->value) {
+$_smarty_tpl->tpl_vars['novidade']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['novidade']->key;
  $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']++;
 ?>
 				  <?php if ($_smarty_tpl->getVariable('smarty')->value['foreach']['foo']['index']==4) {?>
@@ -117,19 +129,19 @@ $_smarty_tpl->tpl_vars['product']->_loop = true;
 						
 						<div class="card">
 						  <a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
+pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['novidade']->value['id'];?>
 "><img src="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-<?php echo $_smarty_tpl->tpl_vars['product']->value['photo'];?>
+<?php echo $_smarty_tpl->tpl_vars['novidade']->value['photo'];?>
 " alt="Avatar" style="width:100%"></a>
 						  <div class="containercard">
-						    <h4><b><?php echo $_smarty_tpl->tpl_vars['product']->value['nome'];?>
+						    <h4><b><?php echo $_smarty_tpl->tpl_vars['novidade']->value['nome'];?>
 </b></h4> 
-						    <p><strong><?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						    <p><strong><?php echo $_smarty_tpl->tpl_vars['novidade']->value['preco'];?>
  €</strong></p> 
 						  </div>
 
-						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
-,<?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['novidade']->value['id'];?>
+,<?php echo $_smarty_tpl->tpl_vars['novidade']->value['preco'];?>
 );" class="botaocompra"><span><i class="fa fa-cart-plus fa-lg"></i></span></button>
 
 						</div>
@@ -140,22 +152,23 @@ pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['product'
 				
 				</div>
 				<div class="linha" style="margin-right: 1%; margin-bottom: 1%">
-				<a href="#" style="float: right; color: black;">Mostrar todas >></a>
+				<a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+pages/products/listProducts.php?ex=2" style="float: right; color: black;">Mostrar todas >></a>
 				</div>
 
 
 		</div></center>
 
-		<center><div class="coluna" style="width: 70%;">
+		<center><div class="coluna" style="width: 100%;">
 				<h2 style="text-align: center;">PROMOÇÕES</h2>
 				<div class="linha">
-				<?php  $_smarty_tpl->tpl_vars['product'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['product']->_loop = false;
+				<?php  $_smarty_tpl->tpl_vars['promocao'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['promocao']->_loop = false;
  $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
- $_from = $_smarty_tpl->tpl_vars['products']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+ $_from = $_smarty_tpl->tpl_vars['promocoes']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
  $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']=-1;
-foreach ($_from as $_smarty_tpl->tpl_vars['product']->key => $_smarty_tpl->tpl_vars['product']->value) {
-$_smarty_tpl->tpl_vars['product']->_loop = true;
- $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['product']->key;
+foreach ($_from as $_smarty_tpl->tpl_vars['promocao']->key => $_smarty_tpl->tpl_vars['promocao']->value) {
+$_smarty_tpl->tpl_vars['promocao']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['promocao']->key;
  $_smarty_tpl->tpl_vars['smarty']->value['foreach']['foo']['index']++;
 ?>
 				  <?php if ($_smarty_tpl->getVariable('smarty')->value['foreach']['foo']['index']==4) {?>
@@ -164,19 +177,19 @@ $_smarty_tpl->tpl_vars['product']->_loop = true;
 						
 						<div class="card">
 						  <a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
+pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['promocao']->value['id'];?>
 "><img src="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-<?php echo $_smarty_tpl->tpl_vars['product']->value['photo'];?>
+<?php echo $_smarty_tpl->tpl_vars['promocao']->value['photo'];?>
 " alt="Avatar" style="width:100%"></a>
 						  <div class="containercard">
-						    <h4><b><?php echo $_smarty_tpl->tpl_vars['product']->value['nome'];?>
+						    <h4><b><?php echo $_smarty_tpl->tpl_vars['promocao']->value['nome'];?>
 </b></h4> 
-						    <p><strong><?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						    <p><strong><?php echo $_smarty_tpl->tpl_vars['promocao']->value['preco'];?>
  €</strong></p> 
 						  </div>
 
-						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
-,<?php echo $_smarty_tpl->tpl_vars['product']->value['preco'];?>
+						            <button onclick="addToCart(<?php echo $_smarty_tpl->tpl_vars['promocao']->value['id'];?>
+,<?php echo $_smarty_tpl->tpl_vars['promocao']->value['preco'];?>
 );" class="botaocompra"><span><i class="fa fa-cart-plus fa-lg"></i></span></button>
 
 						</div>
@@ -187,12 +200,14 @@ pages/products/displayproduct.php?id=<?php echo $_smarty_tpl->tpl_vars['product'
 				
 				</div>
 				<div class="linha" style="margin-right: 1%; margin-bottom: 1%">
-				<a href="#" style="float: right; color: black;">Mostrar todas >></a>
+				<a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+pages/products/listProducts.php?ex=3" style="float: right; color: black;">Mostrar todas >></a>
 				</div>
 
 
 		</div></center>
 	</div>
+	
 
 	<?php echo $_smarty_tpl->getSubTemplate ('common/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
--- templates_c/ba1689ee45b302e7a401bd8c33ca72052e7770c8.file.menu_logged_in.tpl.php
+++ templates_c/ba1689ee45b302e7a401bd8c33ca72052e7770c8.file.menu_logged_in.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2017-01-23 23:35:53
+<?php /* Smarty version Smarty-3.1.15, created on 2017-01-27 12:08:35
          compiled from "/usr/users2/mieec2012/ee12113/public_html/trabalhosSiem/trabalhoPHP-2/templates/common/menu_logged_in.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:14964217455852db922acef2-58941228%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ba1689ee45b302e7a401bd8c33ca72052e7770c8' => 
     array (
       0 => '/usr/users2/mieec2012/ee12113/public_html/trabalhosSiem/trabalhoPHP-2/templates/common/menu_logged_in.tpl',
-      1 => 1485214022,
+      1 => 1485476578,
       2 => 'file',
     ),
   ),
@@ -21,6 +21,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   array (
     'BASE_URL' => 0,
     'USERNAME' => 0,
+    'photo' => 0,
+    'ADMIN' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -28,15 +30,28 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 actions/users/logout.php">Logout</a>
  -->
 
+  <?php if (file_exists(((('../../').('images/users/')).($_smarty_tpl->tpl_vars['USERNAME']->value)).('.jpg'))) {?>
+      <?php $_smarty_tpl->tpl_vars['photo'] = new Smarty_variable(((('../../').('images/users/')).($_smarty_tpl->tpl_vars['USERNAME']->value)).('.jpg'), null, 0);?>
+  <?php } elseif (file_exists(((('../../').('images/users/')).($_smarty_tpl->tpl_vars['USERNAME']->value)).('.png'))) {?>
+      <?php $_smarty_tpl->tpl_vars['photo'] = new Smarty_variable(((('../../').('images/users/')).($_smarty_tpl->tpl_vars['USERNAME']->value)).('.png'), null, 0);?>
+  <?php } else { ?>
+      <?php $_smarty_tpl->tpl_vars['photo'] = new Smarty_variable(('../../').('images/users/default.png'), null, 0);?>
+  <?php }?>
+
 <nav id="navigation">
             <ul class="parent">
-              <li id="username"><?php echo $_smarty_tpl->tpl_vars['USERNAME']->value;?>
-
+              <li class="headerProfile" id="username" style="float: right; margin-right: 15%"><img style="position:relative; top:25px; width: auto; height: 60px;" src="<?php echo $_smarty_tpl->tpl_vars['photo']->value;?>
+"><div style="margin-bottom: 0px;"><br><?php echo $_smarty_tpl->tpl_vars['USERNAME']->value;?>
+</div>
                 <ul class="child">
                   <li><a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 pages/users/showProfile.php">Perfil</a></li>
                   <li><a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 pages/orders/showOrders.php">Encomendas</a></li>
+                  <?php if ($_smarty_tpl->tpl_vars['ADMIN']->value) {?>
+                    <li><a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+pages/users/adminSettings.php">Definições Administrador</a></li>
+                  <?php }?>
                   <li><a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 actions/users/logout.php">Logout</a></li>
                 </ul>
